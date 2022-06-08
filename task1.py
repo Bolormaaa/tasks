0734b14916cfa71c720761d6e66a4324d15d997a
Bolormaaa <e.bolmaa@mn.data-artist.com>
2022-06-08 16:36:41 +0800

    task3

--- task1.py
+++ task1.py
@@ -1,2 +1,14 @@
-print("hi")
-print("hello")
+number1 = input("Please enter first number:")
+number1= int(number1)
+print(f'You entered {number1} and its type is {type(number1)}')
+
+number2 = input("Please enter second number:")
+number2=int(number2)
+print(f'You entered {number2} and its type is {type(number2)}')
+
+product=number1*number2
+if product<=1000:
+    print(product)
+else:
+    print(number1+number2)
+20
--- task2.py
+++ task2.py
@@ -1,1 +1,10 @@
-print(9)
+previous_num = 0
+
+for i in range(10):
+    if i>previous_num:
+        current_num = i
+        
+        print(f'previous number = {previous_num}, current number = {current_num}, sum = {current_num + previous_num}')
+
+    previous_num = i
+   
